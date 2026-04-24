20df678ecdffa3d75a694431c39c4b3675b30e27
ginluv21 <gingers1over.g@gmail.com>
2026-04-24 12:24:09 +0700

    0792-BINARY-SEARCH Stats: Time: 0 ms (100%), Space: 8.9 MB (32.89%) - LeetHub

--- 0792-binary-search/0792-binary-search.go
+++ 0792-binary-search/0792-binary-search.go
@@ -1,15 +1,15 @@
 func search(nums []int, target int) int {
-    r, l := 0, len(nums) - 1
+    l, r := 0, len(nums) - 1
 
-    for r <= l {
-        mid := l + (r - l)/2
+    for l <= r {
+        mid := r + (l - r)/2
 
         if nums[mid] == target {
             return mid
         } else if nums[mid] < target {
-            r = mid + 1
+            l = mid + 1
         } else {
-            l = mid - 1
+            r = mid - 1
         }
     }
 
