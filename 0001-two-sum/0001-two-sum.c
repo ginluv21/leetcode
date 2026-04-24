8b1b05d5b40f17062fbc194d6ee4eddbd93564b9
ginluv21 <gingers1over.g@gmail.com>
2026-04-24 15:09:27 +0700

    0001-TWO-SUM Stats: Time: 4 ms (89.5%), Space: 9.7 MB (11.78%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.c
@@ -0,0 +1,62 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+#define SIZE 1007
+
+typedef struct Node {
+    int val;
+    int key;
+    struct Node* next;
+} Node;
+
+Node* table[SIZE];
+
+int hash(int key) {
+    if(key < 0) key = -key;
+    return key % SIZE;
+}
+
+void insert(int key, int val) {
+    int h = hash(key);
+    Node *node = malloc(sizeof(Node));
+
+    node->key = key;
+    node->val = val;
+    node->next = table[h];
+    table[h] = node;
+}
+
+Node* find(int key){
+    int h = hash(key);
+    Node *cur = table[h];
+
+    while(cur){
+        if(cur->key == key) return cur;
+        cur = cur->next;
+    }
+    return NULL;
+}
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    for(int i = 0; i < SIZE; i++){
+        table[i] = NULL;
+    }
+
+    for(int i = 0; i < numsSize; i++){
+        int need = target -  nums[i];
+        Node* n = find(need);
+
+        if(n){
+            int* res = malloc(2 * sizeof(int));
+            res[0] = n->val;
+            res[1] = i;
+            *returnSize = 2;
+            return res;
+        }
+
+        insert(nums[i], i);
+    }
+    *returnSize = 0;
+    return NULL;
+
+}
